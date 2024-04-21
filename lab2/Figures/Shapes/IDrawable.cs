98c33f1b1c557c43fd161bfedf5755871cd13bee
Vladislav8653 <125541794+Vladislav8653@users.noreply.github.com>
2024-04-21 18:42:35 +0300

    Delete lab2 directory

--- lab2/Figures/Shapes/IDrawable.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Figures.Shapes
-{
-    public interface IDrawable
-    {
-        /* public Pen Pen { get; set; }
-         public Color Color { get; set; }
-         public Graphics graphics { get; set; }*/
-    }
-}
